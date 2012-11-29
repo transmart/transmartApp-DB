2fea89de02b4f29e2f8a507bbfcb8129729c02fc
Terry-Weymouth <weymouth@umich.edu>
2012-11-29 12:27:28 -0500

    Corrected encoding on DB and bad character (0x92) in biomart/data/BIO_EXPERIMENT.sql

--- postgresql/transmart-db.sql
+++ postgresql/transmart-db.sql
@@ -2,7 +2,6 @@ DROP DATABASE transmart;
 
 CREATE DATABASE transmart
   WITH OWNER = postgres
-       ENCODING = 'SQL_ASCII'
        TABLESPACE = pg_default
        LC_COLLATE = 'C'
        LC_CTYPE = 'C'

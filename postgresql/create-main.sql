89dda83ce612b8f97e0e91f5f571769bedab1687
Gustavo Lopes <gustavo@thehyve.nl>
2013-05-30 17:53:20 +0200

    Altering search_path comes after creating schemas

--- postgresql/create-main.sql
+++ postgresql/create-main.sql
@@ -5,3 +5,6 @@
 \i biomart/start.sql
 \i deapp/start.sql
 \i ./i2b2_prerequisites.sql
+
+ALTER ROLE biomart_user IN DATABASE transmart SET search_path=searchapp,biomart,deapp,i2b2demodata,
+	i2b2metadata, i2b2hive, i2b2pm, i2b2workdata, public;

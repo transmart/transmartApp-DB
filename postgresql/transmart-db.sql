5aa390ad2e09c4dc6d12714246a23c4835844be2
Terry-Weymouth <weymouth@umich.edu>
2013-06-11 11:19:56 +0100

    updated comment in transmart-db.sql about sql for terminating outstanding connections

--- postgresql/transmart-db.sql
+++ postgresql/transmart-db.sql
@@ -1,5 +1,5 @@
 -- to kill all sessions connectioned to the database
--- select pg_terminate_backend(procpid) from pg_stat_activity where datname='transmart';
+-- select pg_terminate_backend(pid) from pg_stat_activity where datname='transmart';
 
 -- DROP DATABASE transmart;
 

0156458c51214e58d812a1483b9908c4ee2e072d
Ibrahim Emam <i.emam@imperial.ac.uk>
2013-07-19 16:35:45 +0100

    Fixes for ETL

--- postgresql/transmart-db.sql
+++ postgresql/transmart-db.sql
@@ -1,9 +1,11 @@
 -- to kill all sessions connectioned to the database
--- select pg_terminate_backend(pid) from pg_stat_activity where datname='transmart';
 
--- DROP DATABASE transmart;
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'transmart';
+
+DROP DATABASE transmart;
 
 CREATE DATABASE transmart
-  WITH OWNER = postgres
+  WITH OWNER = postgres TEMPLATE template0
        TABLESPACE = pg_default
+       ENCODING='UTF-8' LC_COLLATE='en_US.UTF-8'
        CONNECTION LIMIT = -1;
